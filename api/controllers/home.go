b62d8dea27583c129648cbeb3738e02904b3b94f
Adji Pangestu <adji.pangestu@detik.com>
2020-04-18 06:50:56 +0700

    revision home

--- api/controllers/home.go
+++ api/controllers/home.go
@@ -9,12 +9,16 @@ import (
 type Author struct {
 	Name    string	`json:"name"`
 	Jobs   	string	`json:"jobs"`
+	Url_Prov   	string	`json:"provinsi"`
+	Url_Dunia   	string	`json:"dunia"`
 }
 
 func (server *Server) Home(w http.ResponseWriter, r *http.Request)  {
 	author := Author{
 		Name:	"Adji Pangestu",
 		Jobs:	"Software Enginerr at detikcom",
+		Url_Prov:	"/provinsi",
+		Url_Dunia:	"/all",
 	}
 
 	var data []byte
---
+++ readme.md
@@ -0,0 +1,3 @@
+# API Covid-19
+
+source (https://api.kawalcorona.com/)
